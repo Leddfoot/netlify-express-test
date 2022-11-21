e6a265df540adfed708c4520b03934713a3eaa6e
Thomas Godwin <leddfoot36@gmail.com>
2022-11-21 15:31:35 +0100

    adding some test routes

--- src/api.js
+++ src/api.js
@@ -21,6 +21,54 @@ router.get('/test', (req, res) => {
 app.use('/.netlify/functions/api', router)
 
 
+////////////////////////////////////////////
+////////////////////////////////////////////
+app.get('/homestore', (req, res) => {
+    if (!req.query.id) {
+        return res.send({
+            error: 'You must provide an id in the query! IE: localhost:3000/vinmonopolet?id=blabla. (in the future Marty)'
+        })
+    }
+    const searchTerm = req.query.id
+  
+    fetchHomeStore(searchTerm, (error, storeData) => {
+      if (error) {
+          return res.send({ error })
+      }
+  
+        res.send({
+            storeData: storeData,          
+            storeID: req.query.id,//note this is the query field in the browser, whatever is typed after ?id=blabla inthe browser
+            
+        })
+    })
+  
+  })
+  app.get('/vinmonopolet', (req, res) => {
+    if (!req.query.city) {
+        return res.send({
+            error: 'You must provide a city in the query! IE: localhost:3000/vinmonopolet?city=blabla. (in the future Marty)'
+        })
+    }
+  
+    const searchTerm = req.query.city
+  
+    fetchVinmonopolet(searchTerm, req.query.getallstores, (error, storeData) => {
+      if (error) {
+          return res.send({ error })
+      }
+  
+        res.send({
+            storeData: storeData,          
+            cityFromBrowserQuery: req.query.city//note this is the query field in the browser, whatever is typed after ?address=blabla inthe browser
+        })
+    })
+  
+  })
+////////////////////////////////////////////
+////////////////////////////////////////////
+
+
 
 
 module.exports.handler = serverless(app)
