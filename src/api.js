db61cdb2d2055a0dfccb1ecf73ea94b8dc310dc7
Thomas Godwin <leddfoot36@gmail.com>
2022-11-21 15:47:35 +0100

    added sample code to verify that Node is running on Netlify second time

--- src/api.js
+++ src/api.js
@@ -1,81 +1,77 @@
-const express = require('express')
-const serverless = require('serverless-http')
-const app = express()
+const express = require("express");
+const serverless = require("serverless-http");
+const app = express();
 
 ////for verifying that node is operable on netlify
-const path = require('path')
-const port = process.env.PORT || 3000
+const path = require("path");
+const port = process.env.PORT || 3000;
 
-const router = express.Router()
+const router = express.Router();
 
-router.get('/', (req, res) => {
-    res.json({
-        'hello' : 'hi'
-    })
-})
+router.get("/", (req, res) => {
+  res.json({
+    hello: "hi",
+  });
+});
 
-router.get('/test', (req, res) => {
-    res.json({
-        'hello' : 'test'
-    })
-})
+router.get("/test", (req, res) => {
+  res.json({
+    hello: "test",
+  });
+});
 
-app.use('/.netlify/functions/api', router)
-
+app.use("/.netlify/functions/api", router);
 
 ////////////////////////////////////////////
 ////////////////////////////////////////////
-router.get('/homestore', (req, res) => {
-    if (!req.query.id) {
-        return res.send({
-            error: 'You must provide an id in the query! IE: localhost:3000/vinmonopolet?id=blabla. (in the future Marty)'
-        })
-    }
-    const searchTerm = req.query.id
-  
-    fetchHomeStore(searchTerm, (error, storeData) => {
-      if (error) {
-          return res.send({ error })
-      }
-  
-        res.send({
-            storeData: storeData,          
-            storeID: req.query.id,//note this is the query field in the browser, whatever is typed after ?id=blabla inthe browser
-            
-        })
-    })
-  
-  })
-  router.get('/vinmonopolet', (req, res) => {
-    if (!req.query.city) {
-        return res.send({
-            error: 'You must provide a city in the query! IE: localhost:3000/vinmonopolet?city=blabla. (in the future Marty)'
-        })
-    }
-  
-    const searchTerm = req.query.city
-  
-    fetchVinmonopolet(searchTerm, req.query.getallstores, (error, storeData) => {
-      if (error) {
-          return res.send({ error })
-      }
-  
-        res.send({
-            storeData: storeData,          
-            cityFromBrowserQuery: req.query.city//note this is the query field in the browser, whatever is typed after ?address=blabla inthe browser
-        })
-    })
-  
-  })
+router.get("/homestore", (req, res) => {
+    console.log(`Server is up on port ${port}\n`);
+  if (!req.query.id) {
+    return res.send({
+      error:
+        "You must provide an id in the query! IE: localhost:3000/vinmonopolet?id=blabla. (in the future Marty)",
+    });
+  }
+  const searchTerm = req.query.id;
 
+  fetchHomeStore(searchTerm, (error, storeData) => {
+    if (error) {
+      return res.send({ error });
+    }
 
-  app.listen(port, function () {
+    res.send({
+      storeData: storeData,
+      storeID: req.query.id, //note this is the query field in the browser, whatever is typed after ?id=blabla inthe browser
+    });
+  });
+});
+
+router.get("/vinmonopolet", (req, res) => {
+  if (!req.query.city) {
+    return res.send({
+      error:
+        "You must provide a city in the query! IE: localhost:3000/vinmonopolet?city=blabla. (in the future Marty)",
+    });
+  }
+
+  const searchTerm = req.query.city;
+
+  fetchVinmonopolet(searchTerm, req.query.getallstores, (error, storeData) => {
+    if (error) {
+      return res.send({ error });
+    }
+
+    res.send({
+      storeData: storeData,
+      cityFromBrowserQuery: req.query.city, //note this is the query field in the browser, whatever is typed after ?address=blabla inthe browser
+    });
+  });
+});
+
+app.listen(port, function () {
   console.log(`Server is up on port ${port}\n`);
-})
+});
 ////////////////////////////////////////////
 ////////////////////////////////////////////
 
-
-
-
-module.exports.handler = serverless(app)
+module.exports.handler = serverless(app);
